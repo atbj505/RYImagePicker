7b3c75cafa08cb566ddc00eb5612d75b322ab00d
atbj505 <atbj505@hotmail.com>
2016-06-15 20:22:01 +0800

    add imageManager

---
+++ RYImagePicker/RYImagePicker/RYImagePicker/Model/RYImageManager.h
@@ -0,0 +1,13 @@
+//
+//  RYImageManager.h
+//  RYImagePicker
+//
+//  Created by Robert on 16/6/15.
+//  Copyright © 2016年 Robert. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RYImageManager : NSObject
+
+@end
---
+++ RYImagePicker/RYImagePicker/RYImagePicker/Model/RYImageManager.m
@@ -0,0 +1,13 @@
+//
+//  RYImageManager.m
+//  RYImagePicker
+//
+//  Created by Robert on 16/6/15.
+//  Copyright © 2016年 Robert. All rights reserved.
+//
+
+#import "RYImageManager.h"
+
+@implementation RYImageManager
+
+@end
